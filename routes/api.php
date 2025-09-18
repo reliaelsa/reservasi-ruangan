52e0739f237f470c39ec79332c901dc952b1e12a
SahaHungkul <mhasbysyabani27@gmail.com>
2025-09-18 21:39:26 +0700

    feat add Logout

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/auth/login', [LoginController::class, 'login']);
+Route::post('/auth/register', [RegisterController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/auth/logout', [LogoutController::class, 'logout']);
+});
